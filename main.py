ba8e7cac3dfa45535267f3f0cb20ac9b4e4f650e
richarda23 <richard.c.adams@infinityworks.com>
2025-07-20 20:50:14 +0100

    - various bugfixes
    -- remove date ranges when using year wildcard
    -- fix default date syntax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
-from typing import Dict
+import os
+from typing import Dict, List
 
 from fastmcp import FastMCP
 import edfestcli
@@ -17,7 +18,7 @@ def edinburgh_festival_venues(
     postcode: str = None,
     name: str = None,
     year: str = "2025",
-) -> Dict:
+) -> List[Dict]:
     """
     Searches Edinburgh festival venues.
     :param festival: The type of festival to search for venues in.
@@ -40,30 +41,32 @@ def edinburgh_festival_venues(
 
 @mcp.tool()
 def edinburgh_festival_events(
-    datetime_from="2025-09-01",
-    datetime_to="2025-09-01",
+    datetime_from="2025-01-01 00:00:00",
+    datetime_to="2025-12-31 23:59:59",
     festival="international",
     genre=None,
     venue_name=None,
     search_text=None,
     title=None,
     artist=None,
-) -> Dict:
+    year="*",
+) -> List[Dict]:
     """
     Searches this year's Edinburgh festival events.
     :param datetime_from: An optional ISO8601-like timestamp. e.g. '2025-08-12 00:00:00'
     :param datetime_to:  An optional ISO8601-like timestamp. e.g. '2025-08-12 00:00:00'
-    :param festival:  possible values are fringe, demofringe, jazz, book, international, tattoo, art, hogmanay, science, imaginate, film, mela, storytelling]
+    :param festival:  possible values are fringe, demofringe, jazz, book, international, tattoo, art, hogmanay, science, imaginate, film, mela, storytelling.
     :param genre: The genre of the show. This will vary by festival type but may include comedy, theatre etc
     :param venue_name: The genre of the show. This will vary by festival type but may include comedy, theatre etc
     :param search_text:  description  of the show to search for.
     :param artist:  Name of an artist or performer to search for.
     :param title: the title of the show to search for.
+    :param year: The year of the festival. Defaults to "*", which means all years.
     :return:
     """
     params = {
         "festival": festival,
-        "year": "2025",
+        "year": year,
         "date_from": datetime_from.replace("T", " "),
         "date_to": datetime_to.replace("T", " "),
         "genre": genre,
@@ -72,6 +75,10 @@ def edinburgh_festival_events(
         "artist": artist,
         "title": title,
     }
+    ## If year is "*", we remove the date filters
+    if year == "*":
+        del params["date_from"]
+        del params["date_to"]
     filtered_params = {k: v for k, v in params.items() if v}
 
     results = cli.events(filtered_params)
